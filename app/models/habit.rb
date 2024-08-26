155bd90ffa35085875c491eec0c4b7084cae1619
takumi-ono0154 <156082398+takumi-ono0154@users.noreply.github.com>
2024-08-26 16:46:02 +0900

    ブランチ変更

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,12 +1,7 @@
 class Habit < ApplicationRecord
-  belongs_to :user, optional: true  # optional: trueでuser_idのバリデーションを一時的に無効化
+  belongs_to :user
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :target_date, presence: true
   validates :target_frequency, presence: true
-
-  # ユーザー登録が必要な機能を使うときにuser_idのバリデーションを有効にするメソッド
-  def require_user!
-    validates :user_id, presence: true
-  end
 end
