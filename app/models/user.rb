2189f436ba2b27aac3d86903dc08cdcd2754a1b9
takumi-ono0154 <156082398+takumi-ono0154@users.noreply.github.com>
2024-08-30 14:33:31 +0900

    古いusersテーブル削除

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_many :habits
-
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, length: { maximum: 255 }
-  validates :crypted_password, presence: true
-  validates :salt, presence: true
-end
