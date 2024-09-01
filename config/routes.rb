e52b762b2e0c7149660e0b46eb9b3273525a0eaf
takumi-ono0154 <156082398+takumi-ono0154@users.noreply.github.com>
2024-09-01 14:28:04 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#top"
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  get 'users/:id', to: 'users#show', as: 'user'
+  
   resources :habits do
     collection do
       post "confirm"
