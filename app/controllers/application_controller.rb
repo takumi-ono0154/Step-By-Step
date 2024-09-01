bd0c05cd8266a3b0d6c49537e5136ba775b05ed9
takumi-ono0154 <156082398+takumi-ono0154@users.noreply.github.com>
2024-09-01 15:11:23 +0900

    ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
