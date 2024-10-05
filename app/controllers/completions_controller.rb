49fbdc2db0a6e9a4c6aa147d3bd6dce423bdcf0b
takumi-ono0154 <156082398+takumi-ono0154@users.noreply.github.com>
2024-10-05 16:31:35 +0900

    flash_message作成

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -3,10 +3,7 @@ class CompletionsController < ApplicationController
     @habit = Habit.find(params[:habit_id])
     @completion = @habit.completions.build(user: current_user, completed_at: Date.today)
 
-    if @completion.save
-      redirect_to habits_path, notice: "習慣を達成しました！"
-    else
-      redirect_to habits_path, alert: "エラーが発生しました。"
-    end
+    @completion.save
+    redirect_to habits_path, notice: "お疲れ様でした！"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice
+      "bg-green-400"  # 成功メッセージ
+    when :alert
+      "bg-red-500"    # 警告メッセージ
+    when :error
+      "bg-yellow-500" # エラーメッセージ
+    else
+      "bg-gray-500"   # その他
+    end
+  end
+
+  # Deviseのフラッシュメッセージに対応する背景色を取得するメソッドを追加
+  def devise_flash_background_color(type)
+    case type.to_sym
+    when :signed_in, :signed_out, :updated, :signed_up
+      "bg-green-500"  # 成功メッセージ
+    when :already_signed_out, :not_found, :not_locked, :already_confirmed
+      "bg-yellow-500" # 注意メッセージ
+    when :invalid, :inactive, :locked, :unconfirmed
+      "bg-red-500"    # エラーメッセージ
+    else
+      flash_background_color(type) # その他はデフォルトの色を使用
+    end
+  end
 end
