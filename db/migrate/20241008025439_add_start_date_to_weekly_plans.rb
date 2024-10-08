489f276316f44d66b5392a030b5b6bf5f5cf2785
takumi-ono0154 <156082398+takumi-ono0154@users.noreply.github.com>
2024-10-08 14:56:49 +0900

    マイグレーション修正

--- db/migrate/20241008025439_add_start_date_to_weekly_plans.rb
+++ db/migrate/20241008025439_add_start_date_to_weekly_plans.rb
@@ -1,5 +1,7 @@
 class AddStartDateToWeeklyPlans < ActiveRecord::Migration[7.2]
   def change
-    add_column :weekly_plans, :start_date, :date, null: false
+    add_column :weekly_plans, :start_date, :date, null: true
+    WeeklyPlan.update_all(start_date: Date.today)
+    change_column_null :weekly_plans, :start_date, false
   end
 end
